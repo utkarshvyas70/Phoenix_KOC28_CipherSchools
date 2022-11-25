ca1bec22498f30caf0e578a0034d5533ec6fe20c
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-11-25 23:51:10 +0530

    Email Slicer

--- emailslicer.py
+++ emailslicer.py
@@ -1,5 +1,5 @@
-print("\nWelcome:)\n")
-n=int(input("How many Emails you want to slice: "))
+print("\nWELCOME:)\n")
+n=int(input("How many Emails You Wants To Slice: "))
 for i in range(1,n+1):
     n=str(input("\nEmail: "))
     l=len(n)
@@ -12,10 +12,10 @@ for i in range(1,n+1):
         a=""
         for i in range(0,d):
             a+=n[i]
-            print("User Name: ",a,end="")
-            q=""
-            for j in range(d+1,len(n)):
-                q+=n[j]
-            print("\nDomain: ",q,end="")
-            print()
+        print("User Name: ",a,end="")
+        q = ""
+        for j in range(d+1,len(n)):
+            q+=n[j]
+        print("\nDomain: ",q,end="",)
+        print()
 print("\nThank You :)")
